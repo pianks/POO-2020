c30e463adb0ed4e434cc9698866e39c2a4883e1f
Pianks <vinnicius@alu.ufc.br>
2020-11-24 09:54:03 -0300

    Meu hello world em Java

---
+++ Hello.java
@@ -0,0 +1,18 @@
+
+package hello;
+
+/**
+ *
+ * @author pianc
+ */
+public class Hello {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello World");
+    }
+    
+}
